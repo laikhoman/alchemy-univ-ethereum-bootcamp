7f5543d8a215ad51dcc35d70b4fbca0d965d55c5
laikhoman <realmnature@gmail.com>
2023-12-18 15:34:37 +0700

    week1

---
+++ week1/server/index.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const secp = require("ethereum-cryptography/secp256k1");
+const { keccak256 } = require("ethereum-cryptography/keccak");
+const {toHex, utf8ToBytes} = require("ethereum-cryptography/utils")
+
+const app = express();
+const cors = require("cors");
+
+const port = 3042;
+
+app.use(cors());
+app.use(express.json());
+
+const balances = {
+  "0x123": 100,
+  "0x234": 50,
+  "0x345": 75,
+};
+
+app.get("/balance/:address", (req, res) => {
+  const { address } = req.params;
+  const balance = balances[address] || 0;
+  res.send({ balance });
+});
+
+app.post("/send", async (req, res) => {
+  const { sender, recipient, amount, signature, recovery } = req.body;
+
+  if(!signature) res.status(404).send({ message: "signature dont was provide" });
+  if(!recovery) res.status(400).send({ message: "recovery dont was provide" });
+
+  try {
+    
+    const bytes = utf8ToBytes(JSON.stringify({ sender, recipient, amount }));
+    const hash = keccak256(bytes);
+
+    const sig = new Uint8Array(signature);
+
+    const publicKey = await secp.recoverPublicKey(hash, sig, recovery);
+
+    if(toHex(publicKey) !== sender){
+      res.status(400).send({ message: "signature no is valid" });
+    }
+
+    setInitialBalance(sender);
+    setInitialBalance(recipient);
+
+    if (balances[sender] < amount) {
+      res.status(400).send({ message: "Not enough funds!" });
+    } else {
+      balances[sender] -= amount;
+      balances[recipient] += amount;
+      res.send({ balance: balances[sender] });
+    }
+  } catch (error) {
+    console.log(error.message)
+  }
+  
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}!`);
+});
+
+function setInitialBalance(address) {
+  if (!balances[address]) {
+    balances[address] = 0;
+  }
+}
